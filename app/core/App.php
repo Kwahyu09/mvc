c34a5ca18e4ff7a88e183a18948bdd2b997de312
Kwahyu09 <krisnawahyudi2017@gmail.com>
2023-02-10 23:20:33 +0700

    belajar routing menambahkan file htaccess untuk url

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,21 @@
 class App{
     public function __construct()
     {
-        echo "Ok!";
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseUrl(){
+        if( isset($_GET['url'])){
+            //rtrim untuk menghilangkan / diakhir url
+            $url = rtrim($_GET['url'], '/');
+
+            //membersihkan url dari karakter basing
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+
+            //URL dipecah berdasarkan tanda /
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
